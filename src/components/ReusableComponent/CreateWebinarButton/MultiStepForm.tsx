d42a3248094e446973df0c95e8b5fe7cd572342f
Mestaysser <mestaysserasma@gmail.com>
2025-09-29 17:22:05 +0100

    working on MultiStepForm

--- src/components/ReusableComponent/CreateWebinarButton/MultiStepForm.tsx
+++ src/components/ReusableComponent/CreateWebinarButton/MultiStepForm.tsx
@@ -1,5 +1,7 @@
 import { useWebinarStore } from '@/store/useWebinarStore'
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+
 
 type Step = {
     id: string
@@ -17,12 +19,36 @@ type Props = {
 const MultiStepForm = ({ steps, onComplete }: Props) => {
 
   const {formData, validateStep, isSubmitting, setSubmitting, setModalOpen } = useWebinarStore()
+
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const [completedSteps, setCompletedSteps] = useState<string[]>([])
+  const [validationError, setValidationError] = useState<string | null>(null)
+
+  const currentStep = steps[currentStepIndex]
+  const isFirstStep = currentStepIndex === 0
+  const isLastStep = currentStepIndex === steps.length - 1
+
+
   return <div className="flex flex-col justify-center items-center bg-[#27272A]/20 border border-border rounded-3xl overflow-hidden max-w-6xl mx-auto backdrop-blur-[106px]">
     <div className="flex items-center justify-start">
       <div className="w-full md:w-1/3 p-6">
         <div className="space-y-6">
           {steps.map((step,index) => {
-            1/52/18
+            const isCompleted = completedSteps.includes(step.id)
+            const isCurrent = index === currentStepIndex
+            const isPast = index < currentStepIndex
+
+            return(
+              <div key={step.id} className="relative">
+                <div className="flex items-start gap-4">
+                  <div className="relative">
+                    <motion.div>
+                      hello worlk
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
+            )
           })}
         </div>
       </div>
